ded3d4b601b5f38fb6db0c3e34c2b2b84abb91c4
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-03 18:34:02 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
